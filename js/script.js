8d5dfe3e21f955a13c7198eb85122d94f63855aa
nero <omarm@Nero>
2022-07-07 21:27:55 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 const responsiveNavbar = (function() {
     const button = document.querySelector("#menuButton");
-    const navbar = document.querySelector("#navbar");
+    const navbar = document.querySelector("#navbar")
     button.addEventListener("click", function() {
         if (navbar.className === "navbar") {
-            navbar.className += "navbarResponsive";
+            navbar.className += " navbarResponsive";
         } else {
             navbar.className = "navbar";
         }
@@ -15,20 +15,23 @@ if (document.getElementById('hearderSlide')) {
     $('#hearderSlide').multislider('pause');
 }
 
+
 function closeCart() {
-    const cart = document.querySelector('.productsOnCart');
+    const cart = document.querySelector('.producstOnCart');
     cart.classList.toggle('hide');
-    document.querySelector('body').classList.toggle('stopScrolling');
+    document.querySelector('body').classList.toggle('stopScrolling')
 }
 
+
 const openShopCart = document.querySelector('.shoppingCartButton');
-openShopCart.addEventListener("click", () => {
-    const cart = document.querySelector('.productsOnCart');
+openShopCart.addEventListener('click', () => {
+    const cart = document.querySelector('.producstOnCart');
     cart.classList.toggle('hide');
     document.querySelector('body').classList.toggle('stopScrolling');
 });
 
+
 const closeShopCart = document.querySelector('#closeButton');
 const overlay = document.querySelector('.overlay');
-closeShopCart.addEventListener("click", closeCart);
-overlay.addEventListener("click", closeCart);
+closeShopCart.addEventListener('click', closeCart);
+overlay.addEventListener('click', closeCart);
